049a06f503dbf9bdf28a8b0c376f2146c883a4e9
davybureau <davy.bureau@gmail.com>
2018-01-02 17:27:33 +0100

    Commit with lightly optimized algo

--- ProcessParity.py
+++ ProcessParity.py
@@ -1,7 +1,15 @@
-def process_parity(input_word):
+def naive_process_parity(input_word):
 
     parity_result = input_word & 1
     while input_word:
         input_word = input_word >>1
 
     return parity_result
+
+
+def lightly_optimized_process_parity(input_number):
+    result = 0
+    while input_number:
+        result ^= 1
+        input_number &= input_number - 1
+    return result
