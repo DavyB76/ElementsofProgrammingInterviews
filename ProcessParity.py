4ab453f1b5dadc2936b5f92734bcc9cd8dd5d39f
davybureau <davy.bureau@gmail.com>
2018-01-02 18:04:45 +0100

    test for parity commit

--- ProcessParity.py
+++ ProcessParity.py
@@ -13,3 +13,21 @@ def lightly_optimized_process_parity(input_number):
         result ^= 1
         input_number &= input_number - 1
     return result
+
+
+def parity(x):
+    result = 0
+    while x:
+        result ^= x & 1
+        x >>= 1
+    return result
+
+PRECOMPUTED_PARITY = [parity(i) for i in range(1 << 16)]
+
+def fully_optimized_process_parity(input_number):
+    MASK_SIZE = 16
+    BIT_MASK = 0xFFFF
+    return (PRECOMPUTED_PARITY[input_number >> (3 * MASK_SIZE)] ^
+            PRECOMPUTED_PARITY[(input_number >> (2 * MASK_SIZE)) & BIT_MASK] ^
+            PRECOMPUTED_PARITY[(input_number >> MASK_SIZE) & BIT_MASK] ^
+            PRECOMPUTED_PARITY[input_number & BIT_MASK])
