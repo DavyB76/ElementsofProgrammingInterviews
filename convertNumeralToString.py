56b491517e6fba88217fcde98af53617764ce6f9
davybureau <davy.bureau@gmail.com>
2018-01-02 18:59:07 +0100

    First test for parity commit

---
+++ convertNumeralToString.py
@@ -0,0 +1,24 @@
+map_numeral_to_string = {   0 : "0",
+                            1 : "1",
+                            2 : "2",
+                            3 : "3",
+                            4 : "4",
+                            5 : "5",
+                            6 : "6",
+                            7 : "7",
+                            8 : "8",
+                            9 : "9"
+}
+
+
+def convertNumeralToString(input_number):
+    result_string = ""
+
+    while input_number > 9:
+        modulo_10 = input_number % 10
+        result_string = map_numeral_to_string[modulo_10] + result_string
+        input_number = (input_number - modulo_10) / 10
+
+    result_string = map_numeral_to_string[input_number] + result_string
+
+    return result_string
