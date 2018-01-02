f50e1e18179f6ea947c67f332f6bbbf3fdbcba03
davybureau <davy.bureau@gmail.com>
2018-01-02 18:33:28 +0100

    First test for numeral/string commit

--- mainTests.py
+++ mainTests.py
@@ -1,4 +1,10 @@
 import unittest
 
+from Exercice_4_1_ComputingTheParityOfAWord import Test_4_1_ComputingTheParityOfAWord
+from Exercice_6_1_InterconvertStringsAndIntegers import Test_6_1_InterconvertStringsAndIntegers
+
+Test_4_1_ComputingTheParityOfAWord()
+Test_6_1_InterconvertStringsAndIntegers()
+
 if __name__ == "__main__":
     unittest.main()
